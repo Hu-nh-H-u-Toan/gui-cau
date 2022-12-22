710caef5cf3a105d9230fcd5f57e35ca5cac4e42
huynhtoan315 <85815038+huynhtoan315@users.noreply.github.com>
2022-12-22 08:44:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "Hihi :>>",
-    introTitle: 'Trần Minh Khuê',
+    introTitle: 'Lê Thị Kim Hoàng',
     introDesc: `Cậu có biết tại sao lá cây lại màu xanh ?
     Cậu có biết tại sao gió đến rồi lại bay nhanh ?`,
     btnIntro: '^^HiHi^^',
-    title: 'Cậu có muốn đi xem phim với tớ chứ 🥰',
+    title: 'Cậu có muốn đi chơi với tớ chứ 🥰',
     desc: '🥺🥺 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Vậy cậu muốn đi thứ mấy nà <3',
+    question: 'Vậy cậu muốn đi ngày nào trong những ngày 29 30 32 1 2 <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Thứ 3 nhá :>>',
+    reply: '30 nhá :>>',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: '7h, đến hẹn mình qua đón đi chơi nha.',
+    messDesc: ' Đến hẹn mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100016784772915' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
